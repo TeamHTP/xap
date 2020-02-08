8de98fb22d9237c7ec91b299e542121ddfd0e6ee
marshingjay <jam605@gmail.com>
2020-02-08 01:34:14 -0600

    heroku stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
@@ -7,17 +7,17 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
+@app.route('/authorize', methods= ['GET','POST'])
+def authorize():
+    content = request.json
+   
+    print(content)
+    return False
+    
 
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(host='localhost', debug=True)
+
+
+ 
