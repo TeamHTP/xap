df1a33989f28ed8ab3b344c377349e2005ba1216
Carter Nelson <cartern@ksu.edu>
2020-02-08 01:49:07 -0600

    stuff

--- main.py
+++ main.py
@@ -18,7 +18,5 @@ def authorize():
 
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host='localhost', debug=True)
-
-
+    app.run(threaded=True, port=5000)
  
